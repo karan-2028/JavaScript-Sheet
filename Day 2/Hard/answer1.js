16e3f24a8f62730f1159c1b694ff4ef6ac2d39cd
karan-2028 <karankumar25642@gmail.com>
2023-01-30 20:25:47 +0530

    day 4

--- Day 2/Hard/answer1.js
+++ Day 2/Hard/answer1.js
@@ -1,1 +1,6 @@
+let ch='Love is the best thing in this world.\
+ Some found their love and some are still\
+looking for their love.'
 
+
+// console.log(ch.match('love'))
---
+++ Day 2/Medium/answer12.js
@@ -0,0 +1,4 @@
+let ch='You cannot end a sentence with because \
+because because is a conjunction'
+
+console.log(ch.substr(31,54))
